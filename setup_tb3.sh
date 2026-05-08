d5643612554a6336d39dd3f33f972176135b083c
ozcascante <osvaldo.cascanteaguilar.2026@mumail.ie>
2026-05-08 15:48:52 +0100

    patrolling house working

--- setup_tb3.sh
+++ setup_tb3.sh
@@ -7,6 +7,7 @@ source install/setup.bash
 export ROS_DOMAIN_ID=11
 export TURTLEBOT3_MODEL=waffle
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(ros2 pkg prefix turtlebot3_gazebo)/share/turtlebot3_gazebo/models/
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 
 # Improves Gazebo performance
 export LIBGL_ALWAYS_SOFTWARE=1
@@ -18,6 +19,7 @@ echo "TURTLEBOT3_MODEL: $TURTLEBOT3_MODEL"
 echo "GAZEBO_MODEL_PATH: $GAZEBO_MODEL_PATH"
 echo "LIBGL_ALWAYS_SOFTWARE: $LIBGL_ALWAYS_SOFTWARE"
 echo "SVGA_VGPU: $SVGA_VGPU"
+echo "RMW_IMPLEMENTATION: $RMW_IMPLEMENTATION"
 
 
 # Keep the shell open with the variables active
