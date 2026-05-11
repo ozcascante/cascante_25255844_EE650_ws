4013a2a2a80ba5bebe74f6d83929f6e6148fe95e
ozcascante <osvaldo.cascanteaguilar.2026@mumail.ie>
2026-05-12 00:18:24 +0100

    Added startgazaebo, startrviz and startpatrol aliases

--- setup_tb3.sh
+++ setup_tb3.sh
@@ -15,6 +15,11 @@ export LIBGL_ALWAYS_SOFTWARE=1
 export SVGA_VGPU=0
 #export QT_QPA_PLATFORM=xcb
 
+# ocascante custom Aliases to run Gazebo, Rviz and the project
+alias startgazebo='ros2 launch turtlebot3_house_navigation random_spawn_house.launch.py'
+alias startrviz='ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=./turtlebot3_house_navigation/maps/house_explored.yaml'
+alias startpatrol='ros2 launch turtlebot3_house_navigation patrol_example_launch.py use_sim_time:=True'
+
 # Print variables to verify
 echo "ROS_DOMAIN_ID: $ROS_DOMAIN_ID"
 echo "TURTLEBOT3_MODEL: $TURTLEBOT3_MODEL"
@@ -22,5 +27,7 @@ echo "GAZEBO_MODEL_PATH: $GAZEBO_MODEL_PATH"
 echo "LIBGL_ALWAYS_SOFTWARE: $LIBGL_ALWAYS_SOFTWARE"
 echo "SVGA_VGPU: $SVGA_VGPU"
 echo "RMW_IMPLEMENTATION: $RMW_IMPLEMENTATION"
+echo "aliases: startgazebo, startrviz, startpatrol"
 echo "Environment loaded."
+echo "1. startgazebo, 2. startrviz, 3. startpatrol"
 
